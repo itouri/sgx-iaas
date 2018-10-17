7688d118f1b9567cdd87bff304a43e2934b65fae
itouri <k-akiyama@axsh.net>
2018-10-17 17:48:29 +0900

    void -> char

--- cmd/sgx/ra/sp/client.h
+++ cmd/sgx/ra/sp/client.h
@@ -7,10 +7,10 @@
 
 int run_graphene_vm_ocall(sgx_enclave_id_t *graphene_eid, uuid_t image_id);
 
-int send_to_ras_ocall(void *src, size_t sz);
+int send_to_ras_ocall(char *src, size_t sz);
 
 // RSAからのreceive
-int recv_from_ras_ocall(void **dest, size_t *sz);
+int recv_from_ras_ocall(char **dest, size_t *sz);
 
 // localfileのread
 int read_file_ocall(unsigned char *dest, char *file, off_t *len);
